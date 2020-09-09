7086e6155662f47fc92fd5d292bb3bbe2762292e
Bolin <clement.bolin@epitech.eu>
2020-09-09 08:47:43 +0200

    update isntall.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+if [[ $EUID -ne 0 ]]; then
+    echo "You must run this with superuser priviliges.  Try \"sudo ./install\"" 2>&1
+    exit 1
+else
+    echo "Installing gitStat-go..."
+fi
 make build
 sudo cp ./bin/gitStat-go /usr/local/bin
 echo "gitStat-go has been successfully installed"
